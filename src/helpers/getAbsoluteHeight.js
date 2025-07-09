b753a60f7a7ade80a99bca82e932b71be9209200
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-09 23:59:12 +0300

    feat: add helper getAbsoluteHeight

---
+++ src/helpers/getAbsoluteHeight.js
@@ -0,0 +1,10 @@
+function getAbsoluteHeight(el) {
+    el = (typeof el === 'string') ? document.querySelector(el) : el;
+
+    var styles = window.getComputedStyle(el);
+    var margin = parseFloat(styles['marginTop']) +
+                parseFloat(styles['marginBottom']);
+
+    return Math.ceil(el.offsetHeight + margin);
+}
+export default getAbsoluteHeight;
